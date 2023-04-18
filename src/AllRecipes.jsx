2e8ff14238987cf6b78b2c082ae9c41ecdc82efd
Jack Coots <jack.coots@newtoneurope.com>
2023-04-18 12:20:54 +0100

    Search working in All Recipes

--- src/AllRecipes.jsx
+++ src/AllRecipes.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
 import RecipeList from "./RecipeList";
 import Search from "./Search";
 
 export default function AllRecipes({ myRecipes }) {
+  const [userSearch, setUserSearch] = useState("");
+  const [recipesToRender, setRecipesToRender] = useState(undefined);
+  const handleChange = (event) => setUserSearch(event.target.value);
+
+  useEffect(() => {
+    setRecipesToRender(myRecipes);
+  }, [myRecipes]);
+
+  //   console.log(myRecipes[0]["name"].toLowerCase()); //.indexOf(String(userSearch).toLowerCase())
+
+  let recipes;
+  if (recipesToRender) {
+    if (userSearch) {
+      recipes = myRecipes.filter(
+        (recipe) =>
+          recipe["name"].toLowerCase().indexOf(userSearch.toLowerCase()) > -1 ||
+          recipe["cuisine"].toLowerCase().indexOf(userSearch.toLowerCase()) >
+            -1 ||
+          recipe["category"].toLowerCase().indexOf(userSearch.toLowerCase()) >
+            -1
+      );
+    } else {
+      recipes = recipesToRender;
+    }
+  }
+
+  //   console.log(recipes);
+
+  //   let recipesToRender = myRecipes;
+
   return (
     <div>
-      <Search />
-      <RecipeList myRecipes={myRecipes} />
+      <Search userSearch={userSearch} handleChange={handleChange} />
+      <RecipeList myRecipes={recipes} />
     </div>
   );
 }
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,9 +1,15 @@
 import "./Search.css";
 
-export default function Search() {
+export default function Search(props) {
   return (
+    // userSearch={userSearch} handleChange={handleChange}
+
     <div>
-      <input placeholder="Search for recipes"></input>
+      <input
+        placeholder="Search for recipes"
+        value={props.userSearch}
+        onChange={props.handleChange}
+      ></input>
     </div>
   );
 }
